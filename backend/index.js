3dedaee2a40df72fedf8ff46518cc9100537f262
asif-daffodil <abir@dipti.com.bd>
2025-08-24 10:07:22 +0600

    feat: use PORT from environment variables for server listening

--- backend/index.js
+++ backend/index.js
@@ -60,6 +60,6 @@ app.use('/payments', paymentRouter);
 const adminRouter = require('./router/adminRouter');
 app.use('/admin', adminRouter);
 
-app.listen(4000, () => {
-    console.log('Server is running on port 4000');
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 })
